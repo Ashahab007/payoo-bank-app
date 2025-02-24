df6d732dbbdf8f00f7b56d4ec8f8cbc6b3126a60
Ashahab007 <ashahab007@gmail.com>
2025-02-25 02:18:39 +0600

    first commit

---
+++ script/login.js
@@ -0,0 +1,21 @@
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    // console.log(event);
+    event.preventDefault(); //preventDefault is used for prevent the data erasing from input field after click login button.
+    const accountNumber = document.getElementById("account-number").value;
+    // console.log(accountNumber);
+    const pin = document.getElementById("pin").value;
+    const convertedPin = parseInt(pin);
+    console.log(pin);
+    if (accountNumber.length === 11) {
+      console.log("account number ok");
+      if (convertedPin === 1234) {
+        window.location.href = "./main.html"; //open the expected page
+      } else {
+        alert("Invalid Pin");
+      }
+    } else {
+      alert("Invalid account number");
+    }
+  });
